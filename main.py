5b84f317f8b5e69c5e0748aa2e4616c23713b8fa
Ariel Weiss <relweiss111@gmail.com>
2020-08-23 11:02:33 +0300

    Added Node class skeleton

--- main.py
+++ main.py
@@ -27,7 +27,47 @@ class Node:
         self.neighbors = []
         self.width = width
         self.total_rows = total_rows
-
+    ## ---------------------- ##
+    ##    Getters functions   ##
+    ## ---------------------- ##
     def get_pos(self):
         return self.row, self.col
 
+    def is_closed(self):
+        return self.color == RED
+    def is_open(self):
+        return self.color == GREEN
+    def is_barrier(self):
+        return self.color == BLACK
+    def is_start(self):
+        return self.color == ORANGE
+    def is_end(self):
+        return self.color == TURQUOISE
+    ## ---------------------- ##
+    ## Change state functions ##
+    ## ---------------------- ##
+    def reset(self):
+        return self.color == WHITE
+    def make_close(self):
+        self.color = RED
+    def make_open(self):
+        self.color = GREEN
+    def make_barrier(self):
+        self.color = BLACK
+    def make_start(self):
+        self.color = ORANGE
+    def makr_end(self):
+        self.color = TURQUOISE
+    def make_path(self):
+        self.color = PURPLE
+    ## ---------------------- ##
+    ##  The drawing function  ##
+    ## ---------------------- ##
+    def draw(self,win):
+        pygame.draw.rect(win,self.color,(self.x,self.y,self.width,self.width))
+
+    def update_neighbors(self,grid):
+        pass
+
+    def __lt__(self, other):
+        return False
